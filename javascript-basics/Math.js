b2b7155b17de703d52787af093d536488f996dd6
Tushar0i <171576246+Tushar0i@users.noreply.github.com>
2025-09-20 13:16:45 +0530

    Math methods in js

---
+++ javascript-basics/Math.js
@@ -0,0 +1,9 @@
+console.log(Math.round(5.52)); // 6 
+console.log(Math.round(5.49)); // 5 
+console.log(Math.ceil(90.345)); // 91
+console.log(Math.floor(9.962)); // 9
+console.log(Math.random()); // a random value
+console.log(Math.max(2,390,45,442)); // 442
+console.log(Math.min(2,390,45,442)); // 2
+console.log(Math.pow(9,3)); // 729
+console.log(Math.sqrt(225)); // 15
