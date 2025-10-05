2f689a30439fd6d4552bf7541596c0d2381d57c5
Tushar0i <171576246+Tushar0i@users.noreply.github.com>
2025-10-05 20:57:37 +0530

    added a signin route with jwt verification

--- database-mdb/basic-signup-signin/index.js
+++ database-mdb/basic-signup-signin/index.js
@@ -1,5 +1,5 @@
-// the problem statement is that we have to make 3 routes for the user
-// where the person can signup - signin - and get all other user
+// the problem statement is that we have to make 2 routes for the user
+// where the person can /signup - /signin 
 // we have to store the data in a data base
 
 const express = require("express")
@@ -13,8 +13,7 @@ const PORT = 5000
 
 app.use(express.json())
 
-                 //replace this link with your own 
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.mongodb.net/demoDB")
+mongoose.connect("mongodb+srv://<replace with your username>:<replace with your password>@cluster0.fqid6vu.mongodb.net/demoDatabase")
 
 const User = mongoose.model('Users', {
     name: String,
@@ -28,6 +27,11 @@ const userSchema = z.object({
     password: z.string().min(8).max(25),
 })
 
+const userLoginSchema = z.object({
+    email: z.email(),
+    password: z.string().min(8).max(25)
+})
+
 app.post('/signup', async (req, res) => {
     const credintials = req.body.credintials;
     const response = userSchema.safeParse(credintials);
@@ -37,7 +41,7 @@ app.post('/signup', async (req, res) => {
             msg: "invalid email or password",
         })
     } else {
-    
+
         const existingUser = await User.findOne({ email: response.data.email })
 
         if (existingUser) {
@@ -50,11 +54,48 @@ app.post('/signup', async (req, res) => {
             password: response.data.password,
         })
         user.save()
-        res.json({
-            msg: "User created successfully"
+        let token = jwt.sign({ email: response.data.email }, jwtPassword)
+        return res.json({
+            msg: "User created successfully",
+            token,
         })
     }
 
+})
+
+
+app.post('/signin', async (req, res) => {
+    const credintials = req.body.credintials;
+    const response = userLoginSchema.safeParse(credintials);
+
+    if (!response.success) {
+        res.status(411).json({
+            msg: "invalid email or password",
+        })
+    } else {
+        const existingUser = await User.findOne({ email: response.data.email })
+        if (!existingUser) {
+            return res.status(400).send("User don't exist")
+        } else {
+            const token = req.headers.authorization
+            try {
+                const decoded = jwt.verify(token, jwtPassword)
+                const username = decoded.email
+                if (username === response.data.email) {
+                    res.json({
+                        msg: "Signed in successfully"
+                    })
+
+                }
+            }
+
+            catch (error) {
+                res.json({
+                    msg: "Authentication failed"
+                })
+            }
+        }
+    }
 })
 
 
