c27d670aef12e207dade7af8d20792a5b3391051
Tushar0i <171576246+Tushar0i@users.noreply.github.com>
2025-10-13 21:33:55 +0530

    Intro to react

---
+++ react/react-intro/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+// const { useState } = require('react') do the same thing as above but we have to make some file change to use the import from thing
+import './App.css'
+
+// async function getData(link) {
+//     const response = await fetch(link)
+//     const data = response.json()
+//     console.log(data)
+//     return data.number
+// } just trying something never mind
+
+function App() {
+
+    const [count, setCount] = useState(0)
+    // const [variabe, function] = useState(input)
+    // variable = input 
+    // a function to do thing with the variable
+
+    
+    return (
+        <>
+            <div>
+                <button onClick={() => setCount(function (count) {
+                    return count + 1
+                })}>
+                    count is {count}
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default App
