2722403f622c65b5177d04c96c0d978916cee5bf
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-19 21:27:49 +0530

    Re-renders 1

--- react/react-intro/src/App.jsx
+++ react/react-intro/src/App.jsx
@@ -10,27 +10,36 @@ import './App.css'
 // } just trying something never mind
 
 function App() {
-
-    const [count, setCount] = useState(0)
+    const [firstTitle , setFirstTitle ] = useState("This is me a random number "+Math.random())
     // const [variabe, function] = useState(input)
     // variable = input 
     // a function to do thing with the variable
 
+
+    // any time a parent re-render all its childeren re-renders 
+    // when ever the setFirstTitle get called the whole App will re-render which is not a good thing  
+    function changeTitle(){
+        console.log("function reached change title")
+        setFirstTitle("This is me a random number "+Math.random())
+    }
     
     return (
         // this thing can only return a top level xml
         // reason - makes it easy to do reconciliation
         // this is why we have included <> and </>
         <>
-            <div>
-                <button onClick={() => setCount(function (count) {
-                    return count + 1
-                })}>
-                    count is {count}
-                </button>
-            </div>
+            <button onClick={changeTitle}> Click me to change the random number</button>
+            <Header title={firstTitle}></Header>
+            <Header title={'This is me Tushar.'} ></Header>
+            <Header title={'Just learning react a day before Diwali far from home in a hostal.'}></Header>
         </>
     )
 }
 
+function Header({title}){
+    console.log("function reached Header")
+    return <div>
+        {title}
+    </div>
+}
 export default App
