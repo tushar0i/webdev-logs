16c3602b198241377dc76e4caa2b5a14bedf3b29
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-19 21:50:56 +0530

    Pushing the state down to reduce the re-render

--- react/react-intro/src/App.jsx
+++ react/react-intro/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 // } just trying something never mind
 
 function App() {
-    const [firstTitle , setFirstTitle ] = useState("This is me a random number "+Math.random())
+    // const [firstTitle , setFirstTitle ] = useState("This is me a random number "+Math.random())
     // const [variabe, function] = useState(input)
     // variable = input 
     // a function to do thing with the variable
@@ -18,26 +18,40 @@ function App() {
 
     // any time a parent re-render all its childeren re-renders 
     // when ever the setFirstTitle get called the whole App will re-render which is not a good thing  
-    function changeTitle(){
-        console.log("function reached change title")
-        setFirstTitle("This is me a random number "+Math.random())
-    }
-    
+    // as a solution to this we will push the state down 
+
+    // function changeTitle(){
+    //     console.log("function reached change title")
+    //     setFirstTitle("This is me a random number "+Math.random())
+    // }
+
     return (
         // this thing can only return a top level xml
         // reason - makes it easy to do reconciliation
         // this is why we have included <> and </>
         <>
-            <button onClick={changeTitle}> Click me to change the random number</button>
-            <Header title={firstTitle}></Header>
+            {/* <button onClick={changeTitle}> Click me to change the random number</button>
+            <Header title={firstTitle}></Header> */}
+            <HeaderWithButton />
             <Header title={'This is me Tushar.'} ></Header>
             <Header title={'Just learning react a day before Diwali far from home in a hostal.'}></Header>
         </>
     )
 }
 
-function Header({title}){
-    console.log("function reached Header")
+function HeaderWithButton() {
+    const [firstTitle, setFirstTitle] = useState("This is me a random number " + Math.random())
+
+    function changeTitle() {
+        setFirstTitle("This is me a random number " + Math.random())
+    }
+    return <>
+        <button onClick={changeTitle}> Click me to change the random number</button>
+        <Header title={firstTitle}></Header>
+    </>
+}
+
+function Header({ title }) {
     return <div>
         {title}
     </div>
