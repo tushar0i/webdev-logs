f3d6e6ccca772e725300ad6cc55baad28d8a56c4
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-23 15:07:56 +0530

    component wrapper (clean syntax)

--- react/react-intro/src/App-b.jsx
+++ react/react-intro/src/App-b.jsx
@@ -1,23 +1,47 @@
 // component wrapper in react 
 
-function App_b(){
+function App_b() {
     return <>
-    <CardWrapper innerComponent={<TextComponent title={"this is the title that i am passing to the text component"}/>}/>
+        {/* we will never use this syntax */}
+        {/* <CardWrapper innerComponent={<TextComponent title={"this is the title that i am passing to the text component"}/>}/> */}
+
+        {/* mostly this syntax is being used */}
+        <CardWrapper>
+            {/*pass the main logic here */}
+            <TextComponent title={"this is the title that i am passing to the text component"} />
+        </CardWrapper>
+        
+        <CardWrapper>
+            <CardWrapper>
+                <CardWrapper>
+                    <div>
+                        <h2>Just testing the wrapper </h2>
+                    </div>
+                </CardWrapper>
+            </CardWrapper>
+        </CardWrapper>
     </>
 }
 
-function TextComponent({title}){
+function TextComponent({ title }) {
     return <div>
-    <h2>this is the text component which is wrapped inside the wrapper component</h2>
-    <h3>{title}</h3>
+        <h2>this is the text component which is wrapped inside the wrapper component</h2>
+        <h3>{title}</h3>
     </div>
 }
 
-function CardWrapper({innerComponent}){
-    return <div style={{border:"2px solid blue", padding:"20px" , margin:"30px"}}>
+function CardWrapper({ children }) {
+    return <div style={{ border: "2px solid blue", padding: "20px", margin: "30px" }}>
         <h1>hi there I am a wrapper component</h1>
-        {innerComponent}
+        {children}
     </div>
 }
+
+// function CardWrapper({innerComponent}){
+//     return <div style={{border:"2px solid blue", padding:"20px" , margin:"30px"}}>
+//         <h1>hi there I am a wrapper component</h1>
+//         {innerComponent}
+//     </div>
+// }
 
 export default App_b
