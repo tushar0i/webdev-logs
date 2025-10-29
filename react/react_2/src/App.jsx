7aae766fb41cca516954e3b6761444b7b7ff4305
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-10-29 20:34:30 +0530

    Client-side Routing in react

--- react/react_2/src/App.jsx
+++ react/react_2/src/App.jsx
@@ -1,34 +1,41 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+// Client Side Routing
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom" // we will do routing with next.js in future
 import { Landing } from "../Components/Landing"
 import { Dashboard } from "../Components/Dashboard"
 
 function App() {
-    console.log(window.location)
     return (
         <>
-            <div style={{backgroundColor:"lavender"}}>
-                <div style={{ border: "2px solid red", margin: "10px 10px 30px 10px", padding: "5px" , boxShadow:"0px 0px 20px black" }}>
-                    This is the information which is constant in all the pages
-                    <button onClick={()=>{
-                        window.location.href="/"
-                    }}>Landing page</button>
-                    <button onClick={()=>{
-                        // window.location.href="/dashboard" this will do a hard reload and we dont want that to happen this is why we use 
-                    }}>Dashboard page</button>
-                </div>
-                <div>
-
+            <div style={{ backgroundColor: "lavender" }}>
                     <BrowserRouter>
+                    <Appbar/>
                         <Routes>
                             <Route path="/dashboard" element={<Dashboard />} />
                             <Route path="/" element={<Landing />} />
                         </Routes>
                     </BrowserRouter>
-
-                </div>
             </div>
         </>
     )
 }
 
+
+function Appbar() {
+    const navigate = useNavigate()
+    console.log(window.location)
+    return (
+        <>
+            <div style={{ border: "2px solid red", margin: "10px 10px 30px 10px", padding: "5px", boxShadow: "0px 0px 20px black" }}>
+                This is the information which is constant in all the pages
+                <button onClick={() => {
+                    navigate("/")
+                }}>Landing page</button>
+                <button onClick={() => {
+                    navigate("/dashboard")
+                    // window.location.href="/dashboard" this will do a hard reload and we dont want that to happen this is why we use 
+                }}>Dashboard page</button>
+            </div>
+        </>
+    )
+}
 export default App
