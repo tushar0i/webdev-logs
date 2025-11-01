9eca72a02d6f9b60dea19b6f766640843796ac09
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-02 00:34:00 +0530

    Adding css

--- react/state_management/src/App.jsx
+++ react/state_management/src/App.jsx
@@ -1,8 +1,14 @@
 // State Management through Redux
+import ItemList from "./components/itemList"
+import "./App.css"
+
 function App(){
 
     return (<>
-    
+        <div className="mainContainer">
+            <h2>🧾 My Cart</h2>
+            <ItemList/>
+        </div>
     </>)
 }
 
