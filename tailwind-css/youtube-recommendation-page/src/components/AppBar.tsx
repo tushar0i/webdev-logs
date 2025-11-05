458ce289eca4287562a0ddb09ae115b737a90d76
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-05 15:56:35 +0530

    AppBar component

---
+++ tailwind-css/youtube-recommendation-page/src/components/AppBar.tsx
@@ -0,0 +1,17 @@
+
+export const AppBar = () =>{
+
+    return (<>
+    <div className="flex justify-between p-2">
+        <div >
+            <img src="/YouTubelight.svg" className="w-31 flex inline-flex items-center pt-2" alt="" />
+        </div>
+        <div>
+            Search box
+        </div>
+        <div>
+            Sign in
+        </div>
+    </div> 
+    </>)
+}
