0e53dd75dd4f0331e4d6c3c7171d95c21cc7b1d7
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-08 23:35:30 +0530

    Initial commit: testing things

---
+++ tailwind-css/Dashboard-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/Button";
+
+const App = () => {
+    return(
+        <>
+        <Button/>
+        </>
+    )
+}
+
+export default App;
---
+++ tailwind-css/Dashboard-app/src/components/Button.jsx
@@ -0,0 +1,7 @@
+export function Button(){
+    return (<>
+    <div className="bg-red-300">
+        this is a button but it is not a button it's actually a div
+    </div>
+    </>)
+}
