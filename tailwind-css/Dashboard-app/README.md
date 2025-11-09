5181115f38c51be4f253a19527ddffc94c15263c
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-09 23:41:43 +0530

    Adding MainCard Component

--- tailwind-css/Dashboard-app/README.md
+++ tailwind-css/Dashboard-app/README.md
@@ -1,1 +1,2 @@
 ## Dashboard-app
+### fully hard coded
--- tailwind-css/Dashboard-app/src/App.jsx
+++ tailwind-css/Dashboard-app/src/App.jsx
@@ -1,14 +1,24 @@
 import { Button } from "./components/Button";
+import MainCard from "./components/MainCard";
+import PProcessedCard from "./components/PProcessedCard";
 import PriceCard from "./components/PriceCard";
 
 const App = () => {
-    return(
+    return (
         <>
-        <div className="grid grid-cols-3">
-        <PriceCard title={'Amount processed'} orderCount={'13'}  amount={'100,00.20'}></PriceCard>
-        <PriceCard title={'Amount processed'} orderCount={'13'}  amount={'100,00.20'}></PriceCard>
-        <PriceCard title={'Amount processed'} orderCount={'13'}  amount={'100,00.20'}></PriceCard>
-        </div>
+            <div className="flex">
+                <div className="flex-1">
+                    <MainCard title={'Amount processed'} orderCount={'13'} amount={'100,00.20'}></MainCard>
+                </div>
+                <div className="flex-1">
+                    <PriceCard title={'Amount pending'} orderCount={'13'} amount={'100,00.20'}></PriceCard>
+                </div>
+                <div className="flex-1">
+                    <PProcessedCard title={'Amount pending'} amount={'100,00.20'}></PProcessedCard>
+                </div>
+
+            </div>
+
         </>
     )
 }
